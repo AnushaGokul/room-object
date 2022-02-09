b7ac9b862dccb873e0c70b4c0361c47994e03b20
AnushaGokul <71561519+AnushaGokul@users.noreply.github.com>
2022-02-09 17:17:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function bedroom() {
+    window.location = "bedroom.html";
+  }
+  function lamp() {
+    window.location = "lamp.html";
+  }
+  function living_room() {
+    window.location = "living_room.html";
+  }
+  function study_table() {
+    window.location = "study_table.html";
+  }
+  function tv_cabinet() {
+    window.location = "tv_cabinet.html";
+  }
+  
---
+++ tv_cabinet.js
@@ -0,0 +1,45 @@
+
+img = "";
+status = "";
+objects = [];
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectdetector = ml5.objectDetector("cocossd", modelloaded);
+  document.getElementById("status").innerHTML = "status: detecting objects";
+}
+function preload() {
+  img = loadImage("tv cabinet.jpg");
+}
+function draw() {
+  image(img, 0, 0, 640, 420);
+  if (status != "") {
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "status: Object detected";
+
+      fill("#ff0000");
+      percent = floor(objects[i].confidence * 100);
+      text(
+        objects[i].label + "" + percent + "%",
+        objects[i].x + 10,
+        objects[i].y + 20
+      );
+      noFill();
+      stroke("#ff0000");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
+function modelloaded() {
+  console.log("Model loaded");
+  status = true;
+  objectdetector.detect(img, gotresult);
+}
+function gotresult(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+    objects = results;
+  }
+}
